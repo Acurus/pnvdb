4ff3a90155c6d6dfe4c9e10d3dc0191a59e7b03f
Acurus <jtkyrdalen@gmail.com>
2016-09-23 20:36:01 +0200

    progress

--- PVDB.py
+++ PVDB.py
@@ -4,17 +4,32 @@ class PVDBread:
     "Read NVDB data"
     
     def __init__(self):
-        self.baseUrl = 'https://www.vegvesen.no/nvdb/api/v2/'
-        self.headers = {'user-agent': 'kyrdalen.no'}
+        self.baseUrl = 'https://www.vegvesen.no/nvdb/api/v2'
+        self.headers = {"X-Client": "PVDB","X-Kontaktperson": "jankyr@vegvesen.no"}
+        self.segmentation = 'true'
+        self.srid = '32633'
+        
     
     def checkResponse(self, resp):
         if True:
             return resp
 
-    def getRoadObjects(self):
-        url = self.baseUrl
-        data = requests.get(self.baseUrl,headers=self.headers)
-        return self.checkResponse(data.json())
+    def getRoadObjects(self,obj,params):
+        self.paramUpdate = {'segmentering':self.segmentation,'srid':self.srid}
+        urlAdd = 'vegobjekter'
+        url = '{baseUrl}/{urlAdd}/{obj}'.format(baseUrl=self.baseUrl,urlAdd=urlAdd,obj=obj)
+        params.update(self.paramUpdate)
+        print(params)
+        data = requests.get(url, params = params, headers=self.headers).json()
+        return self.checkResponse(data)
+    
+    def getRoadObject(self,obj,objectID):
+        urlAdd = 'vegobjekter'
+        url = '{baseUrl}/{urlAdd}/{obj}/{objectID}'.format(baseUrl=self.baseUrl,
+            urlAdd=urlAdd,obj=obj,objectID=objectID)
+        data = requests.get(url).json()
+        return self.checkResponse(data)
+
 
     def getRoadObjectTypes():
         pass
---
+++ PVDB_exceptions.py
@@ -0,0 +1,19 @@
+
+404 4012    Objekt ble ikke funnet.
+415 4001    Denne mediatypen er ikke støttet.
+422 4000    Noe er galt med brukerens input.
+422 4002    Feil i spesifisering av heltallsverdi.
+422 4003    Feil i spesifisering av heltallslisteverdi.
+422 4004    Manglende obligatorisk felt.
+422 4005    Feil i spesifisering av tekstverdi.
+422 4006    Feil i spesifisering av tekstlisteverdi.
+422 4007    Feil i spesifisering av datoverdi.
+422 4008    Feil i spesifisering av boolsk verdi.
+422 4009    Konflikterende feltverdier spesifisert.
+422 4010    Feil i spesifisering av flyttallsverdi.
+422 4011    Feil i spesifisering av flyttallistesverdi.
+422 4013    Ukjent felt spesifisert.
+422 4014    Dette feltet kan kun forekomme én gang.
+500 5000    Uventet feil har oppstått.
+500 5001    Feil med kommunikasjon med databaser.
+500 5002    Typeregisteret mangler.
