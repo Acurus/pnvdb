b1d0fcce31074888afdf425d00e7de26f49509d9
Kyrdalen Jan Tore <JANKYR@vegvesen.no>
2018-07-16 15:42:07 +0200

    Plukk

--- pnvdb/datafangst.py
+++ pnvdb/datafangst.py
@@ -1,7 +1,36 @@
 from . import config
 import requests
 import geojson
+"""
+{
+"type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [ [10.39241731, 63.43053048],
+            [10.39495434, 63.43043698],
+            [10.39579151, 63.42898665],
+            [10.39272171, 63.42909269],
+            [10.39241731, 63.43053048] ]
+        ]
+      },
+      "properties": {
+        "tag": "Forsterkningslag#1",
+        "dataCatalogVersion": "2.06",
+        "typeId": 227,
+        "comment": "Usikker på måledatoen",
+        "attributes": {
+          "5543": "20160802"
+         }
+       }
+    }
+  ]
+}
 
+"""
 class Datafangst(object):
     def __init__(self, username=None, password=None, contractId=None):
         self.base_url = config.datafangst_base_url
@@ -21,7 +50,8 @@ class FeatureCollection(object):
         self.features = []
     
     def add_feature(self, feature):
-        self.features.append(feature.__repr__())
+        #self.features.append(feature.__repr__())
+        self.features.append(feature)
     
     def push(self):
         feature_collection = geojson.FeatureCollection(self.features)
@@ -38,7 +68,7 @@ class Feature(object):
         self.properties["tag"] = tag
 
     def coordinates(self, geometry):
-        self._coordinates = geojson.Point(geometry)
+        self._coordinates = geojson.Polygon(geometry)
 
     def attribute(self, attribute_id, attribute_value):
         self.properties[str(attribute_id)] = attribute_value
@@ -53,6 +83,10 @@ class Feature(object):
         feature = geojson.Feature(geometry=self._coordinates,
                                   properties=self.properties)
         return geojson.dumps(feature)
+    
+    def feature(self):
+        return geojson.Feature(geometry=self._coordinates,
+                                  properties=self.properties)
         
 
 
--- test_datafangst.py
+++ test_datafangst.py
@@ -1,24 +1,35 @@
 from pnvdb.var import auth
 import pnvdb
-
+import pprint
+import json
+import geojson
 
 if __name__ == '__main__':
+    pp = pprint.PrettyPrinter(indent=4)
     username = auth.username
     password = auth.password
     contractId = '7168c19c-e637-48fd-9771-61eb43c53d6f'
     datafangst = pnvdb.Datafangst(username, password, contractId)
-    feature1 = pnvdb.Feature(96,(1,2),"test_objekt")
-    feature1.attribute(985,'hei')
+    poly = [[10.39241731, 63.43053048],
+            [10.39495434, 63.43043698],
+            [10.39579151, 63.42898665],
+            [10.39272171, 63.42909269],
+            [10.39241731, 63.43053048]]
 
-    feature2 = pnvdb.Feature(96,(2,5),"test_objekt1")
-    feature2.attribute(985,'hadet')
+    feature1 = pnvdb.Feature(227,poly,"Forsterkningslag#1")
+    feature1.attribute(5543,'2016802')
+    feature1.comment("Usikker på maaledatoen")
+
     
-    #print(feature1)
+    
+    #pp.pprint(json.loads(feature1.__repr__()))
+    #print(json.loads(feature1.__repr__()))
+    
 
     datafangst_collection = pnvdb.FeatureCollection()
-    datafangst_collection.add_feature(feature1)
-    datafangst_collection.add_feature(feature2)
-    print(datafangst_collection.push())
+    datafangst_collection.add_feature(feature1.feature())
+    #print(datafangst_collection.push())
+    pp.pprint(datafangst_collection.push())
     
     
 
